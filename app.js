5e0d22c798777eab7f6ab5c9489f4f90b3511547
Omid Mortazavi <omidmor@students.cs.ubc.ca>
2023-09-11 14:33:17 -0700

    fix bug different func name in files

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-// Ensure the DOM is fully loaded before executing our code
 document.addEventListener("DOMContentLoaded", function() {
     const tronWeb = window.tronWeb;
     const contractAddress = "TKc58kSi2VnE2DPoc9QwpqGkNjMCdV9iBo"; // Replace with your deployed contract address
     let contractInstance;
+
+    const connectBtn = document.getElementById("connectWallet");
+    const viewBtn = document.getElementById("viewButton");
 
     // Ensure TronWeb is injected and available
     if (typeof tronWeb === 'undefined') {
@@ -10,29 +12,40 @@ document.addEventListener("DOMContentLoaded", function() {
         return;
     }
 
+    connectBtn.addEventListener("click", async () => {
+        try {
+            // Request user's account address
+            const addresses = await tronWeb.request({ method: 'tron_requestAccounts' });
+            if (addresses.length) {
+                connectBtn.style.display = "none";
+                viewBtn.style.display = "block";
+            }
+        } catch (error) {
+            console.error("Error:", error);
+            alert("Failed to connect TronLink wallet.");
+        }
+    });
+
     tronWeb.contract().at(contractAddress).then(instance => {
         contractInstance = instance;
-        initApp();
     }).catch(error => {
         console.error("Error:", error);
         alert("There was an error fetching the contract instance.");
     });
 
-    function initApp() {
-        document.getElementById("viewButton").addEventListener("click", async () => {
-            try {
-                // User signs and sends 100 TRX to the contract
-                await contractInstance.payAndView().send({
-                    callValue: tronWeb.toSun(100) // Convert 100 TRX to SUN (the smallest unit)
-                });
+    viewBtn.addEventListener("click", async () => {
+        try {
+            // User signs and sends 100 TRX to the contract
+            await contractInstance.clickButton().send({
+                callValue: tronWeb.toSun(100) // Convert 100 TRX to SUN (the smallest unit)
+            });
 
-                // Fetch the updated count
-                const count = await contractInstance.getViewersCount().call();
-                document.getElementById("counterDisplay").innerText = count.toString();
-            } catch (error) {
-                console.error("Error:", error);
-                alert("There was an error processing your request.");
-            }
-        });
-    }
+            // Fetch the updated count
+            const count = await contractInstance.getClickCount().call();
+            document.getElementById("counterDisplay").innerText = count.toString();
+        } catch (error) {
+            console.error("Error:", error);
+            alert("There was an error processing your request.");
+        }
+    });
 });
